a8ccbbe1aa95d521e2e567e444d4a2b4a14c4fc1
Surya Teja <Surya Teja@LAPTOP-N3Q913SL>
2020-11-29 11:19:51 +0530

    programe6 changes

--- src/main/java/com/sit/first/Program6.java
+++ src/main/java/com/sit/first/Program6.java
@@ -1,31 +1,22 @@
 package com.sit.first;
-// Demonstrate throw.
-class Program6 {
-	static void secondMethod() {
-		try {
-			int x = 5/0;
-		} catch(ArithmeticException ae){
-			System.out.println("From ArithmeticException Block");
-			ae.printStackTrace();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+
+public class Program6 {
+
+	public static void main(String[] args) {
+		// To compute the execution time of a class
+
+		long start, end;
+		System.out.println("Timing a for loop from 0 to 100,000,000");
+		// time a for loop from 0 to 100,000,000
+		start = System.currentTimeMillis(); // get starting time
+		System.out.println("Start time: " + start);
+		for (long i = 0; i < 100000000L; i++) // Do nothing Loop
+			;
+		end = System.currentTimeMillis(); // get ending time
+		System.out.println("End time: " + end);
+		
+		System.out.println("Elapsed time: " + (end - start) + " ms");
+
 	}
-	static void throwMethod() {
-		try {
-			throw new NullPointerException("example using demo and rethrow method");
-		} catch(NullPointerException e) {
-			System.out.println("Caught inside throw method.");
-			System.out.println("Throw message from the exception is retrieved by the method e.getMessage and the message is : " + e.getMessage());
-			//throw e; // rethrow the exception
-		}
-	}
-	public static void main(String args[]) {
-		try {
-			throwMethod();
-			secondMethod();
-		} catch(NullPointerException e) {
-			System.out.println("Recaught: " + e);
-		}
-	}
+
 }
